38022a323003fd774a088c5a39117f2708f34d80
choibyungnam <throw@cttd.co.kr>
2017-02-07 16:43:46 +0900

    [study] setting 및 parallax

--- src/C.Parallax/_vendor/library.js
+++
@@ -1,7 +0,0 @@
-/*! library.js */
-// jQuery 등의 외부 라이브러리
-(function () {
-	'use strict';
-
-	console.log('library.js');
-}());
--- src/C.Parallax/js/script.js
+++
@@ -1,9 +0,0 @@
-//= require _common.js
-//= require ../_vendor/library.js
-
-/*! script.js */
-(function () {
-	'use strict';
-
-	console.log('script.js');
-}());
